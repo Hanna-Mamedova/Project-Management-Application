837b603c625436b00ff50357864bbd4a397ceef5
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-11-02 10:43:38 +0100

    refactor: remove boards folder

--- project-management-app/src/app/app.module.ts
+++ project-management-app/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BoardModule } from './boards/board/board.module';
-import { MainModule } from './boards/main/main.module';
+import { BoardModule } from './board/board.module';
+import { MainModule } from './main/main.module';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 
