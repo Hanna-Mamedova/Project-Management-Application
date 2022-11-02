e98ef44acf76826603e2802120d4c486b000cecd
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-11-02 12:10:10 +0100

    fix: change routes to main and board module

--- project-management-app/src/app/app-routing.module.ts
+++ project-management-app/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'main', loadChildren: () => import('./boards/main/main.module').then(m => m.MainModule) },
-  { path: 'board', loadChildren: () => import('./boards/board/board.module').then(m => m.BoardModule) },
+  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+  { path: 'board', loadChildren: () => import('./board/board.module').then(m => m.BoardModule) },
   { path: '**', component: NotFoundComponent },
 ];
 
