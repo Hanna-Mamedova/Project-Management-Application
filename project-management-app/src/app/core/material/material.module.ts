0bb6824cf485ef85853e90e1bdef88c3bea01a78
Pandobeer <nikolyav@gmail.com>
2022-11-04 21:56:20 +0100

    feat: add buttons & styles to boards

--- project-management-app/src/app/core/material/material.module.ts
+++ project-management-app/src/app/core/material/material.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const materialModules = [
   MatCardModule,
   MatButtonModule,
   MatIconModule,
+  MatMenuModule,
+  MatFormFieldModule,
+  MatInputModule,
 ];
 
 @NgModule({
