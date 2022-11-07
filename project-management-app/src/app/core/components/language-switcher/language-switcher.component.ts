a0a12f64152734bb82151a9050d7f7fd4f3dca67
jazzzrabbit <jazzzrabbit@gmail.com>
2022-11-07 21:26:04 +0300

    refactor: change lang switcher component a bit

--- project-management-app/src/app/core/components/language-switcher/language-switcher.component.ts
+++ project-management-app/src/app/core/components/language-switcher/language-switcher.component.ts
@@ -1,28 +1,17 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
-
 @Component({
   selector: 'app-language-switcher',
   templateUrl: './language-switcher.component.html',
   styleUrls: ['./language-switcher.component.scss'],
 })
 export class LanguageSwitcherComponent {
-  languageList = [
-    { code: 'en', label: 'English' },
-    { code: 'ru', label: 'Russian' },
-  ];
-
   lang = 'en';
 
   @ViewChild('flagIcon') flagIcon: ElementRef;
 
   constructor(private translateSevice: TranslateService) { }
-
-  // changeLang(lang: string) {
-  //   console.log(lang);
-  //   this.translateSevice.use(lang);
-  // }
 
   changeLang() {
     if (this.lang === 'en') {
