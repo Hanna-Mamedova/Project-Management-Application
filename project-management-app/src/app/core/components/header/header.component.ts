f8b687c55309aacd327d07b3064ae021e0bbb6e5
jazzzrabbit <jazzzrabbit@gmail.com>
2022-11-08 16:09:12 +0300

    feat: add sticky header

--- project-management-app/src/app/core/components/header/header.component.ts
+++ project-management-app/src/app/core/components/header/header.component.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
+window.addEventListener('scroll', () => {
+  const scrollPosition: number = window.scrollY;
+  const header = document.querySelector('.header') as HTMLElement;
+  const headerBackground = document.querySelector('.block') as HTMLElement;
+
+  if (scrollPosition > 50) {
+    header.classList.add('sticky');
+    headerBackground.classList.add('block--visible');
+  } else {
+    header.classList.remove('sticky');
+    headerBackground.classList.remove('block--visible');
+  } 
+});
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
