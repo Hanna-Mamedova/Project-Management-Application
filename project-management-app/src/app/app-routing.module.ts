ef56711aaf04b757e26457efbecc0c2f4268e972
jazzzrabbit <jazzzrabbit@gmail.com>
2022-11-10 11:17:35 +0300

    feat: add auth guard

--- project-management-app/src/app/app-routing.module.ts
+++ project-management-app/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule), canLoad: [AuthGuard] },
   { path: 'board', loadChildren: () => import('./board/board.module').then(m => m.BoardModule) },
   { path: '**', component: NotFoundComponent },
 ];
