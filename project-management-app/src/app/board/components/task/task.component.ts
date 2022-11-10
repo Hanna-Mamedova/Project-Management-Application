c614d9ec4f6e46a3c94863f2386b8da42e53536a
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-11-10 10:59:26 +0100

    feat: add task actions and reducers

--- project-management-app/src/app/board/components/task/task.component.ts
+++ project-management-app/src/app/board/components/task/task.component.ts
@@ -1,13 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Task } from 'src/app/core/models/interfaces';
+import { Store } from '@ngrx/store';
+import { deleteTask } from 'src/app/core/store/actions/tasks.actions';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.scss'],
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit {
   @Input()
     task: Task;
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+  }
+
+  editTask(id: string): void {
+    console.log('OPEN EDIT MODAL');
+
+  }
+
+  deleteTask(id: string): void {
+    this.store.dispatch(deleteTask({ taskId: id }));
+  }
 
 }
