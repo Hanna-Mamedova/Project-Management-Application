d407e8889fc076509e82d83447fa1c15e8b1e207
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-11-11 19:57:18 +0100

    feat: add board id to board state

--- project-management-app/src/app/core/store/reducers/board.reducers.ts
+++ project-management-app/src/app/core/store/reducers/board.reducers.ts
@@ -5,6 +5,7 @@ import { BoardStateInterface } from '../state.models';
 
 export const initialBoardState: BoardStateInterface = {
   board: {
+    id: '',
     title: '',
     description: '',
     columns: [],
@@ -28,6 +29,7 @@ export const boardReducers = createReducer(
     (state, action): BoardStateInterface => ({
       ...state,
       board: {
+        id: state.board.id,
         title: state.board.title,
         description: state.board.description,
         columns: [...state.board.columns!, action.createdColumn],
@@ -44,6 +46,7 @@ export const boardReducers = createReducer(
       return {
         ...state,
         board: {
+          id: state.board.id,
           title: state.board.title,
           description: state.board.description,
           columns: updatedColumns,
@@ -61,6 +64,7 @@ export const boardReducers = createReducer(
       return {
         ...state,
         board: {
+          id: state.board.id,
           title: state.board.title,
           description: state.board.description,
           columns: updatedColumns,
