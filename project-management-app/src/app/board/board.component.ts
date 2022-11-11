c5a87496ac287222d9b81eccd1aaab3cbff35705
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-11-11 15:34:19 +0100

    refactor: remove mock data

--- project-management-app/src/app/board/board.component.ts
+++ project-management-app/src/app/board/board.component.ts
@@ -13,73 +13,16 @@ import { Column } from '../core/models/interfaces';
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
-  // board: Board = {
-  //   id: '1',
-  //   title: 'TITLE1',
-  //   description: 'dsf',
-  //   columns: [
-  //     {
-  //       id: '1',
-  //       title: 'COLUMN1',
-  //       order: 1,
-  //       tasks: [
-  //         {
-  //           id: '1;',
-  //           title: 'TASK1',
-  //           order: 1,
-  //           description: 'sfdj;sfd',
-  //           userId: 'sdf;j;',
-  //           boardId: 'af;jf',
-  //           columnId: 'sd;fj;',
-  //         },
-  //         {
-  //           id: '2;',
-  //           title: 'TASK2',
-  //           order: 2,
-  //           description: 'sdfsff;sfd',
-  //           userId: 'sd;',
-  //           boardId: 'asdf',
-  //           columnId: 'sdffj;',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       id: '2',
-  //       title: 'COLUMN2',
-  //       order: 2,
-  //       tasks: [
-  //         {
-  //           id: '3;',
-  //           title: 'TASK3',
-  //           order: 1,
-  //           description: 'sfdj;sfd',
-  //           userId: 'sdf;j;',
-  //           boardId: 'af;jf',
-  //           columnId: 'sd;fj;',
-  //         },
-  //         {
-  //           id: '4',
-  //           title: 'TASK4',
-  //           order: 2,
-  //           description: 'sdfsff;sfd',
-  //           userId: 'sd;',
-  //           boardId: 'asdf',
-  //           columnId: 'sdffj;',
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // };
 
   board$ = this.store.select(selectBoard);
 
   columns$ = this.store.select(selectColumns);
 
-  columnIds$ = this.columns$.pipe(map((columns) => columns.map((column: Column) => column.id!)))
+  columnIds$ = this.columns$.pipe(map((columns) => columns.map((column: Column) => column.id!)));
 
   constructor(
     private store: Store<BoardStateInterface>,
-    ) {}
+  ) { }
 
   ngOnInit(): void {
     this.store.dispatch(getBoard());
