7138a7ed22b2ae4c37a95c5fe217383871a8aa34
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-11-15 22:33:58 +0100

    refactor: add initial states ts

---
+++ project-management-app/src/app/core/store/initial-states.ts
@@ -0,0 +1,14 @@
+import { BoardsStateInterface, BoardStateInterface } from "./state.models";
+
+export const initialBoardsState: BoardsStateInterface = {
+  boards: [],
+};
+
+export const initialBoardState: BoardStateInterface = {
+  board: {
+    id: '',
+    title: '',
+    description: '',
+    columns: [],
+  },
+};
