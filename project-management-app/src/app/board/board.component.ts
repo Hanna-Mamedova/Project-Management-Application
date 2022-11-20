abf32bd9e2b524a65dffc0c657b11405fa812bb4
Hanna Kuzich <hanna.kuzich@gmail.com>
2022-11-20 14:44:17 +0100

    refactor: remove console log

--- project-management-app/src/app/board/board.component.ts
+++ project-management-app/src/app/board/board.component.ts
@@ -29,8 +29,6 @@ export class BoardComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    console.log(this.columns$);
-
     this.store.dispatch(getBoard());
     this.showSuccess(Messages.BOARD_LOADED);
   }
